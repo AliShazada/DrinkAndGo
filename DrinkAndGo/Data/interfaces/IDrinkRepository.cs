cb39846c8a5a8a6832158b3226f1c6aeaf2562f8
Ali Ahmad Shazada <aliahmadshahzada@outlook.com>
2019-08-01 19:52:56 +1000

    REPOSITORY INTERFACES

---
+++ DrinkAndGo/Data/interfaces/IDrinkRepository.cs
@@ -0,0 +1,18 @@
+﻿using DrinkAndGo.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DrinkAndGo.Data.interfaces
+{
+    interface IDrinkRepository
+    {
+         IEnumerable<Drink> Drink{ get; set; }
+
+        IEnumerable<Drink> PreferredDrinks { get; set; }
+
+        Drink GetDrinkById (int drinkId );
+    }
+}
+ 
